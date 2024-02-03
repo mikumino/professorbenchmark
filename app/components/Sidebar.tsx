891d0b840cedff122e0ecb5ac258c75dbb0d0f83
kippy <amachorro02@gmail.com>
2024-02-02 22:08:59 -0800

    added sidebar

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,21 @@
+export default function Sidebar() {
+    return (
+        <div className="drawer lg:drawer-open">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+            {/* Page content here */}
+            <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+        </div> 
+        <div className="drawer-side">
+            <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                {/* Sidebar content here */}
+                <li><a>College of Science</a></li>
+                <li><a>College of Agriculture</a></li>
+                <li><a>College of Fishing</a></li>
+                <li><a>College of Horses</a></li>
+            </ul>  
+        </div>
+    </div>
+    )
+}
