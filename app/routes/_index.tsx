2377dc694e0a451bbb3a13024aa0f318350634ae
kippy <amachorro02@gmail.com>
2024-02-03 00:43:07 -0800

    updated color themes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,13 +14,13 @@ export default function Index() {
         <div className="hero-content text-center">
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Welcome to ProfessorBenchmark</h1>
-            <p className="py-6">The easiest way to decide who to enroll with. Get started by taking a look at available courses and professors. Then, get a side-by-side view of two professors' stats.</p>
+            <p className="py-6">The easiest way to decide who to enroll with. Get started by taking a look at available courses and professors, then get a side-by-side view of two professors' stats.</p>
             <div className="flex-center space-x-4">
-            <a href="/courses" role="button" className="btn btn-primary">
+            <a href="/courses" role="button" className="btn bg-green-800">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" /></svg>
               Courses
             </a>
-            <a href="/professors" role="button" className="btn btn-primary">
+            <a href="/professors" role="button" className="btn bg-green-800">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"> <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /> </svg>
               Professors
             </a>
