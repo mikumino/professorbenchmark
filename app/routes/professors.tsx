a544388767b3fc240a0e6a03577c7f50390a2f19
anvilsp <77859117+anvilsp@users.noreply.github.com>
2024-02-02 22:22:59 -0800

    Fix professor page styling oops

--- app/routes/professors.tsx
+++ app/routes/professors.tsx
@@ -2,8 +2,6 @@ import { MetaFunction } from "@remix-run/node";
 import Navbar from "~/components/Navbar";
 import Autocomplete from "~/components/Autocomplete";
 
-const API = "https://api.cppscheduler.com/"
-
 export const meta: MetaFunction = () => {
     return [
       { title: "ProfessorBenchmark" },
@@ -16,7 +14,7 @@ export default function Courses() {
         <>
             <div className="">
                 <Navbar />
-                <div className="max-3-xl flex flex-col mt-64 items-center">
+                <div className="max-3-xl flex flex-col mt-8 items-center">
                     <h1 className="text-4xl font-bold mb-4">Search by professor</h1>
                     <Autocomplete category="professor" />
                 </div>
