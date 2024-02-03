864c31a2b06311f3b835b8df48f1400c6476d45a
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-02-02 18:57:35 -0800

    autocomplete with filtering

--- app/components/Autocomplete.tsx
+++ app/components/Autocomplete.tsx
@@ -1,47 +1,63 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+
+interface Course {
+    name: string;
+}
 
 interface AutocompleteProps {
     category: string
 }
 
-interface Course {
-    subject: string,
-    number: string
-}
-
 export default function Autocomplete(props: AutocompleteProps) {
     const [courses, setCourses] = useState<Course[]>([]);
+    const [searchResults, setSearchResults] = useState<Course[]>([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     async function getCourses() {
         let data = await fetch("https://api.cppscheduler.com/data/courses/findAll", {
             method: "POST",
-          });
+        });
 
         let courses = await data.json();
         courses = courses.map((course: any) => {
-            return ({subject: course.Subject, number: course.CourseNumber})
+            let name = course.Subject + " " + course.CourseNumber
+            return ({name: name})
         });
-
+        console.log('ran')
         setCourses(courses);
     }
 
     useEffect(() => {
         getCourses();
-    }, []);   
-    
-    console.log(courses);
-    
+    }, []);
 
+    const filterCourses = (searchTerm: string) => {
+        const results = courses.filter(course => 
+            course.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+            );
+        setSearchResults(results);
+    }
+
+    const filterProfessors = (searchTerm: string) => {
+        // Fill with professors
+    }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+        if (searchTerm.length > 1) {
+            props.category === "course" ? filterCourses(searchTerm) : filterProfessors(searchTerm);
+        }
+    }
+    
     return (
         <div className="dropdown mb-8">
-            <input className="input input-bordered" placeholder={`Select a ${props.category}`}/>
+            <input className="input input-bordered" placeholder={`Select a ${props.category}`} onChange={handleInputChange}/>
             <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 max-h-80 flex-nowrap overflow-auto">
                 {
-                    // REALLY stupid ternary operator lol
                     props.category === "course" ?
-                    courses.map((course, index) => (
+                    searchResults.map((course, index) => (
                         <li key={index}>
-                            <a href="#" className="hover:bg-primary-100">{course.subject} {course.number}</a>
+                            <a href="#" className="hover:bg-primary-100">{course.name}</a>
                         </li>
                     )) :
                     // Fill with professors
