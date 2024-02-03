078e8dae713e3c356c111e4f9d99648141d64dda
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-02-02 17:59:55 -0800

    max height and scrolling for autocomplete

--- app/components/Autocomplete.tsx
+++ app/components/Autocomplete.tsx
@@ -6,8 +6,9 @@ export default function Autocomplete(props: AutocompleteProps) {
     return (
         <div className="dropdown mb-8">
             <input className="input input-bordered" placeholder={`Select a ${props.category}`}/>
-            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 max-h-80 flex-nowrap overflow-auto">
                 {/* Dynamically fill with whatever info is needed */}
+                
             </ul>
         </div>
     )
