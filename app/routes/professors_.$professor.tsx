432ac10a8221fa75dbb046e6278d0dd1ac45941f
anvilsp <77859117+anvilsp@users.noreply.github.com>
2024-02-03 09:22:19 -0800

    Professor page data doesn't render until loaded

--- app/routes/professors_.$professor.tsx
+++ app/routes/professors_.$professor.tsx
@@ -29,33 +29,43 @@ export default function Professor() {
         fetchProfessorAndClasses();
     }, []);
 
-    return (
-        <>
-            <Navbar />
-            <div className="max-w-3xl mx-auto mt-8">
-                <div className="flex flex-row w-full">
-                    <div className="flex flex-col mr-8 mt-2">
-                        <GradeRadial grade={professorInfo ? professorInfo.AvgGPA : 0} />
-                    </div>
-                    <div className="flex flex-col">
-                        <div className="flex flex-row">
-                            <h1 className="text-4xl font-bold mb-4 mr-4">{professorInfo ? professorInfo.InstructorFirst + " " + professorInfo.InstructorLast : ''}</h1> 
-                            <Link to={professorInfo ? `/compare/${professorInfo.InstructorFirst}$${professorInfo.InstructorLast}` : '/'} className="btn bg-green-800">Compare</Link>
+    if(professorInfo) {
+        return (
+            <>
+                <Navbar />
+                <div className="max-w-3xl mx-auto mt-8">
+                    <div className="flex flex-row w-full">
+                        <div className="flex flex-col mr-8 mt-2">
+                            <GradeRadial grade={professorInfo ? professorInfo.AvgGPA : 0} />
                         </div>
-                        <h1 className="text-2xl mb-4 mr-4 items-start">{professorInfo ? "Average GPA: " + (professorInfo.AvgGPA ? professorInfo.AvgGPA.toFixed(2) : "N/A") : ''}</h1>
+                        <div className="flex flex-col">
+                            <div className="flex flex-row">
+                                <h1 className="text-4xl font-bold mb-4 mr-4">{professorInfo ? professorInfo.InstructorFirst + " " + professorInfo.InstructorLast : ''}</h1> 
+                                <Link to={professorInfo ? `/compare/${professorInfo.InstructorFirst}$${professorInfo.InstructorLast}` : '/'} className="btn bg-green-800">Compare</Link>
+                            </div>
+                            <h1 className="text-2xl mb-4 mr-4 items-start">{professorInfo ? "Average GPA: " + (professorInfo.AvgGPA ? professorInfo.AvgGPA.toFixed(2) : "N/A") : ''}</h1>
+                        </div>
+                    </div>
+                    {/* <h3 className="text-2xl font-bold mb-4">Average GPA: {professorInfo ? professorInfo.AvgGPA.toFixed(2) : ''}</h3> */}
+                    <div className="flex w-full flex-col">
+                        {courses.map((course, index) => {
+                            return (
+                                <ResultRow link={`${course.subject}%20${course.number}`} category={`course`} headerText={`${course.subject} ${course.number}`} subtitleText={course.AvgGPA ? `GPA: ${(course.AvgGPA.toFixed(2))}` : 'N/A'} endItem={<GradeRadial grade={course.AvgGPA} />} key={index} />
+                            )
+                        })}
                     </div>
                 </div>
-                {/* <h3 className="text-2xl font-bold mb-4">Average GPA: {professorInfo ? professorInfo.AvgGPA.toFixed(2) : ''}</h3> */}
-                <div className="flex w-full flex-col">
-                    {courses.map((course, index) => {
-                        return (
-                            <ResultRow link={`${course.subject}%20${course.number}`} category={`course`} headerText={`${course.subject} ${course.number}`} subtitleText={course.AvgGPA ? `GPA: ${(course.AvgGPA.toFixed(2))}` : 'N/A'} endItem={<GradeRadial grade={course.AvgGPA} />} key={index} />
-                        )
-                    })}
-                </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
+    else
+    {
+        return(
+            <>
+                <Navbar />
+            </>
+        )
+    }
 }
 
 
