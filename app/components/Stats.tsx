1e3eadec91bb37454ab40f428398b6d4c0cad75f
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-02-03 10:02:05 -0800

    compare select show stat

--- app/components/Stats.tsx
+++ app/components/Stats.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import GradeRadial from "./GradeRadial"
+import { getProfessorClasses } from "~/utils/apiUtils";
+import ResultRow from "./ResultRow";
 
 interface StatsProps {
     professor: ProfessorInfo
@@ -6,6 +9,16 @@ interface StatsProps {
 }
 
 export default function Stats({professor}: StatsProps) {
+    const [courses, setCourses] = useState<CourseData[]>([]);
+
+    useEffect(() => {
+        const fetchClasses = async () => {
+            setCourses(await getProfessorClasses(professor.InstructorFirst, professor.InstructorLast));
+        };
+
+        fetchClasses();
+    }, []);
+
     return (
         <div className="stats stats-vertical shadow text-center">
             <h1 className="text-4xl mb-4">{professor.InstructorFirst} {professor.InstructorLast}</h1>
@@ -25,7 +38,11 @@ export default function Stats({professor}: StatsProps) {
                 <div className="stat-value">{professor.TotalEnrollment}</div>
                 <div className="stat-desc"></div>
             </div>
-        
+            {courses.map((course, index) => {
+                return (
+                    <ResultRow link={`${course.subject}%20${course.number}`} category={`course`} headerText={`${course.subject} ${course.number}`} subtitleText={course.AvgGPA ? `GPA: ${(course.AvgGPA.toFixed(2))}` : 'N/A'} endItem={<GradeRadial grade={course.AvgGPA} />} key={index} />
+                )
+            })}
         </div>
     )
 }
