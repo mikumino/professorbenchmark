3c325b110c9bc3b6ef4cdfe2aab33ffc8fb113d8
anvilsp <77859117+anvilsp@users.noreply.github.com>
2024-02-02 22:49:19 -0800

    He's back

--- app/components/ResultRow.tsx
+++ app/components/ResultRow.tsx
@@ -8,13 +8,13 @@ export default function ResultRow(props: ResultRowProps) {
         console.log("clicked");
     }
 
-    if(props.result.AvgGPA) {
+    if(props.result.AvgGPA || props.result.Label === "Sen Wang") {
         return (
             <>
                 <div onClick={handleClick} className="btn-ghost rounded-xl hover:cursor-pointer p-4 w-full flex flex-row items-center justify-center">
                     <div className="flex flex-col text-left w-full">
                         <h1 className="text-lg font-bold">{props.category === "course" ? props.result.name : props.result.Label}</h1>
-                        <p className="text-sm">{props.category === "course" ? props.result.CourseTitle : props.result.AvgGPA.toFixed(2)}</p>
+                        <p className="text-sm">{props.category === "course" ? props.result.CourseTitle : (props.result.AvgGPA ? props.result.AvgGPA.toFixed(2) : "N/A")}</p>
                     </div>
                 </div>
                 <div className="divider"></div>
