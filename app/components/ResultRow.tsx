fdf4990c25453740d6aa5ee30a6e47670d37f6ad
anvilsp <77859117+anvilsp@users.noreply.github.com>
2024-02-02 22:22:26 -0800

    Professor shows GPA

--- app/components/ResultRow.tsx
+++ app/components/ResultRow.tsx
@@ -13,7 +13,7 @@ export default function ResultRow(props: ResultRowProps) {
             <div onClick={handleClick} className="btn-ghost rounded-xl hover:cursor-pointer p-4 w-full flex flex-row items-center justify-center">
                 <div className="flex flex-col text-left w-full">
                     <h1 className="text-lg font-bold">{props.category === "course" ? props.result.name : props.result.Label}</h1>
-                    <p className="text-sm">{props.category === "course" ? props.result.CourseTitle : props.result.AvgGPA}</p>
+                    <p className="text-sm">{props.category === "course" ? props.result.CourseTitle : (props.result.AvgGPA ? props.result.AvgGPA.toFixed(2) : "N/A")}</p>
                 </div>
             </div>
             <div className="divider"></div>
